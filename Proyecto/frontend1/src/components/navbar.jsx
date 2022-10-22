aa09c875caa3a694288a7af8564d7688d0e0ec87
Juan Pablo Rojas <jprojaschinchilla@gmail.com>
2022-10-21 23:47:18 -0600

    terminado final

--- Proyecto/frontend1/src/components/navbar.jsx
+++ Proyecto/frontend1/src/components/navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
       <div className='container'>
         <a className='navbar-brand' href='/'>
-          Proyecto 1 Servidor 1
+          Proyecto 1 Funcion Publica
         </a>
         <button
           className='navbar-toggler'
--- Proyecto/frontend2/src/components/navbar.jsx
+++ Proyecto/frontend2/src/components/navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar() {
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
       <div className='container'>
         <a className='navbar-brand' href='/'>
-          Proyecto 1 Servidor 2
+          Proyecto 1 Desarrollo Economico
         </a>
         <button
           className='navbar-toggler'
